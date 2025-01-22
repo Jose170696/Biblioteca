37420c154dcbcdaab1ee45443817a78f466b1aea
Jenicampos96 <jencamb@gmail.com>
2025-01-22 14:59:09 -0600

    Creación de clase LibroPrestado heredando la clase Libro

---
+++ LibroPrestado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class LibroPrestado : Libro
+    {
+        public DateTime FechaPrestamo { get; set; }
+
+        public override void MostrarInfo()
+        {
+            base.MostrarInfo();
+            Console.WriteLine($"Fecha de Préstamo: {FechaPrestamo.ToShortDateString()}");
+        }
+    }
+}
