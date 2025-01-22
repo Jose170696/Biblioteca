f01c19d6d7316aa33531f239e3c3776c47509a0e
Jenicampos96 <jencamb@gmail.com>
2025-01-22 14:47:29 -0600

    Creación de clase Miembro

---
+++ Miembro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Miembro
+    {
+        public string Nombre { get; set; }
+        public string ID { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, ID: {ID}, Fecha de Registro: {FechaRegistro.ToShortDateString()}");
+        }
+    }
+}
