c2681893fd87e047ec9809cd9e1d01566552b578
Jenicampos96 <jencamb@gmail.com>
2025-01-22 13:46:19 -0600

    Creación de clase Libro

---
+++ Libro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Libro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string ISBN { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+
+        public virtual void MostrarInfo()
+        {
+            Console.WriteLine($"Título: {Titulo}, Autor: {Autor}, ISBN: {ISBN}," +
+                $" Fecha de Publicación: {FechaPublicacion.ToShortDateString()}");
+        }
+    }
+}
